05f87f31b75d2972dc0c9b18fe15b022f74433e3
Breno Soares <breno.soares@indexdigital.com.br>
2016-09-28 15:24:36 -0300

    version 1

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,7 +14,7 @@ module.exports = yeoman.Base.extend({
       {
         type: 'input',
         name: 'project_name',
-        message: 'What is your project name? '
+        message: 'What is your project fuck name? '
       },
       {
         type: 'checkbox',
@@ -119,7 +119,10 @@ module.exports = yeoman.Base.extend({
       // STYLE CSS
       this.fs.copyTpl(
         this.templatePath('style.css'),
-        this.destinationPath('app/style.css')
+        this.destinationPath('app/style.css'),
+        {
+          project_name: this.props.project_name
+        }
       );
       // LIB
       this.fs.copyTpl(
